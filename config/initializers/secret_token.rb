31ce599b0ceba95e44b97c50934104a015d70649
Sherief Gharraph <gharraph@msn.com>
2012-07-20 16:14:26 -0700

    just created the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = 'd1914a5700c1f3b2d8f18def09432e2939c2b6b190cc7a9b86d1fed8299be274de516b263fc7489e82d9305803a128d2c68240475ae0ac90ab0c8351a21da48e'
